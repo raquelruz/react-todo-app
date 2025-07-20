d2e7252bfdac033be5d13c703f954670865a433e
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2025-07-20 18:53:05 +0200

    chore(app): add app title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 export const App = () => {
     return (
         <div className="app-container">
-            App
+            <h1 className="app-title">📝 Mi lista de tareas</h1>
         </div>
     );
 };
